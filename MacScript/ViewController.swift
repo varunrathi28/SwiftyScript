8b329ef24fedc37d6b3bb28a83aa0d7d2ca953a5
Varun Rathi <varunrathi28@users.noreply.github.com>
2020-11-26 01:17:28 +0530

    Initial Commit

---
+++ MacScript/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacScript
+//
+//  Created by VR on 26/11/20.
+//  Copyright © 2020 VR. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

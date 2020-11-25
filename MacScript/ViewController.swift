3d48a6994600195e95c9fdc39a945a9a79632837
Varun Rathi <varunrathi28@users.noreply.github.com>
2020-11-26 01:35:26 +0530

    Editor Base UI

--- MacScript/ViewController.swift
+++ MacScript/ViewController.swift
@@ -9,17 +9,24 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet var textView:NSTextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    
+    @IBAction func runClicked(sender:NSButton) {
+        let input = textView.textStorage?.string
+        print(input!)
     }
 
 

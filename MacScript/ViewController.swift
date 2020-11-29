11a14709b54398567e06ae5709dd4c5ccb656bb3
Varun Rathi <varunrathi28@users.noreply.github.com>
2020-11-29 21:44:13 +0530

    1. Sandbox removed 2. Pipe for standard output and error

--- MacScript/ViewController.swift
+++ MacScript/ViewController.swift
@@ -10,27 +10,23 @@ import Cocoa
 
 class ViewController: NSViewController {
     @IBOutlet var textView:NSTextView!
-    @IBOutlet var consoleView:ConsoleView!
+    @IBOutlet var progressIndicator:NSProgressIndicator!
+   // @IBOutlet var consoleView:ConsoleView!
+    @IBOutlet var consoleTextView:NSTextView!
     var inputProcessor = InputProcessor()
     
+    dynamic var isRunning = false
+    var outputPipe:Pipe!
+    var errorPipe:Pipe!
+    var buildTask:Process!
     
     override func viewDidLoad() {
         super.viewDidLoad()
          self.textView.lnv_setUpLineNumberView()
-        //addTestView()
-        
+
         // Do any additional setup after loading the view.
     }
-    
-    func addTestView(){
-        
-       
-        let container = NSTextContainer()
-        let frame = NSRect(x: 500, y: 300, width: 30, height: 30)
-        let textview = ConsoleView(frame: frame, textContainer: container)
-        textView.backgroundColor = .red
-        consoleView.addSubview(textView)
-    }
+
     
     override var representedObject: Any? {
         didSet {
@@ -38,16 +34,37 @@ class ViewController: NSViewController {
         }
     }
     
+    func animateSpinner(_ shouldAnimate:Bool){
+        DispatchQueue.main.async {
+            if shouldAnimate {
+                self.progressIndicator.startAnimation(self)
+            }
+            else{
+                self.progressIndicator.stopAnimation(self)
+            }
+        }
+    }
     
     @IBAction func runClicked(sender:NSButton) {
         let inputText = textView.textStorage?.string
-        consoleView.addToLogs(inputText!)
+        //consoleView.addToLogs(inputText!)
         
-     //   inputProcessor.processInput(input: inputText)
+        let path = inputProcessor.processInput(input: inputText)
+        animateSpinner(true)
+        
+        var arguments = [String]()
+        arguments.append("swift")
+        arguments.append(path)
+        runScript(arguments)
+        
      //   highlightLine(line: 3)
     }
     
-
+    @IBAction func stopClicked(sender:NSButton){
+        animateSpinner(false)
+    }
+    
+    
     func highlightLine(line lineNumberToHighlight: Int) {
         let layoutManager = self.textView.layoutManager!
         var currentLine = 0
@@ -69,6 +86,107 @@ class ViewController: NSViewController {
             glyphIndex = NSMaxRange(range)
             currentLine += 1
         }
+    }
+    
+
+    func runScript(_ arguments:[String]) {
+        
+        //1.
+        isRunning = true
+        
+        let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+        
+        //2.
+        taskQueue.async {
+            
+            //1.
+            guard let path = Bundle.main.path(forResource: "SwiftScript",ofType:"command") else {
+                print("Unable to locate SwiftScript.command")
+                return
+            }
+            
+            //2.
+            self.buildTask = Process()
+            self.buildTask.launchPath = path
+            //self.buildTask.launchPath = "/usr/bin/env"
+            self.buildTask.arguments = arguments
+            
+            //3.
+            self.buildTask.terminationHandler = {
+                
+                task in
+                DispatchQueue.main.async(execute: {
+                   // self.buildButton.isEnabled = true
+                    self.animateSpinner(false)
+                    self.isRunning = false
+                })
+                
+            }
+            
+            self.captureStandardOutputAndRouteToTextView(self.buildTask)
+            
+            //4.
+            self.buildTask.launch()
+            
+            //5.
+            self.buildTask.waitUntilExit()
+            
+        }
+        
+    }
+    
+
+    func captureStandardOutputAndRouteToTextView(_ task:Process) {
+      
+      //1.
+      outputPipe = Pipe()
+      errorPipe = Pipe()
+      task.standardOutput = outputPipe
+      task.standardError = errorPipe
+      
+      //2.
+      outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+      errorPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+      
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.NSFileHandleDataAvailable, object: errorPipe.fileHandleForReading, queue: nil) {
+            notification in
+            
+            let error = self.errorPipe.fileHandleForReading.availableData
+            let errorString = String(data: error, encoding: String.Encoding.utf8) ?? ""
+            
+            DispatchQueue.main.async {
+                let previousOutput = self.consoleTextView.string
+                let nextOutput = previousOutput + "\n Error:" + errorString
+                self.consoleTextView.string = nextOutput
+                let range = NSRange(location:nextOutput.count,length:0)
+                self.consoleTextView.scrollRangeToVisible(range)
+            }
+            
+            self.errorPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        }
+        
+      //3.
+      NotificationCenter.default.addObserver(forName: NSNotification.Name.NSFileHandleDataAvailable, object: outputPipe.fileHandleForReading , queue: nil) {
+        notification in
+        
+        //4.
+        let output = self.outputPipe.fileHandleForReading.availableData
+        let outputString = String(data: output, encoding: String.Encoding.utf8) ?? ""
+        
+        //5.
+        DispatchQueue.main.async(execute: {
+          let previousOutput = self.consoleTextView.string
+          let nextOutput = previousOutput + "\n" + outputString
+          self.consoleTextView.string = nextOutput
+          
+          let range = NSRange(location:nextOutput.count,length:0)
+          self.consoleTextView.scrollRangeToVisible(range)
+          
+        })
+        
+        //6.
+        self.outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+      }
     }
     
 }
